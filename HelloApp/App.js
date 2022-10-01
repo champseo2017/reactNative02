6bfedc9304a2ddbeabdde2246aeb0bfaeac9f29b
ChampKos <32265715+champseo2017@users.noreply.github.com>
2022-10-01 17:48:31 +0700

    การกำหนด StyleSheet
    - ควรสร้าง StyleSheet แยก จากนั้นค่อยมากำหนดให้
        คอมโพเนนต์
    - กำหนด style ให้แตกต่างกันระหว่าง Android และ iOS
          - Platform

--- HelloApp/App.js
+++ HelloApp/App.js
@@ -1,22 +1,68 @@
 import React from 'react';
 
-import {View, Text} from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Platform
+} from 'react-native';
 
 const App = () => {
   /* 
-    View 
-    ใช้เป็น Container เพื่อกำหนดขอบเขตในการแสดงผล
-    ภายใน View สามารถเพิ่มคอมโพเนนต์อื่นๆ ได้
+    การกำหนด StyleSheet
 
-    Text ใช้แสดงข้อความ ใน React ต้องแสดงข้อความในคอม
-    โพเนนต์ Text เท่านั้น
+    <Text style={{
+      color: 'blue'
+    }}></Text>
+
+    - รูปแบบที่ต้องกำหนดเป็นตัวเลข ระบุเฉพาะตัวเลข
+    ไม่ต้องใส่หน่วย React Native กำหนดหน่วยที่เหมาะสมให้
+
+    <Text style={{
+      padding: 10,
+      fontSize: 20
+    }}>
+      Hello
+    </Text>
+
+    - ควรสร้าง StyleSheet แยก จากนั้นค่อยมากำหนดให้
+    คอมโพเนนต์
+
+    - กำหนด style ให้แตกต่างกันระหว่าง Android และ iOS
+      - Platform
   
   */
+
   return (
-    <View>
-      <Text>Hello World</Text>
+    <View style={styles.container}>
+      <Text style={styles.textTitle}>Hello World</Text>
+      <Text style={styles.textPlain}>Hello World</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'lightgray',
+    padding: 20,
+    ...Platform.select({ /* ต้องมี (Spread) ข้างหน้า platform */
+      android: {
+        paddingTop: 30,
+        ios: {
+          paddingTop: 45
+        }
+      }
+    })
+  },
+  textTitle: {
+    color: 'blue',
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginTop: 15,
+  },
+  textPlain: {
+    fontSize: 16,
+  },
+});
 
 export default App;
