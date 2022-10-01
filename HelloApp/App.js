2d1d76f407f0a673c3d43b4a0149c2f9ac865c3a
ChampKos <32265715+champseo2017@users.noreply.github.com>
2022-10-01 19:10:51 +0700

    การกำหนด StyleSheet
    - กำหนด Style แบบ Array

--- HelloApp/App.js
+++ HelloApp/App.js
@@ -11,58 +11,43 @@ const App = () => {
   /* 
     การกำหนด StyleSheet
 
-    <Text style={{
-      color: 'blue'
-    }}></Text>
-
-    - รูปแบบที่ต้องกำหนดเป็นตัวเลข ระบุเฉพาะตัวเลข
-    ไม่ต้องใส่หน่วย React Native กำหนดหน่วยที่เหมาะสมให้
-
-    <Text style={{
-      padding: 10,
-      fontSize: 20
-    }}>
-      Hello
-    </Text>
-
-    - ควรสร้าง StyleSheet แยก จากนั้นค่อยมากำหนดให้
-    คอมโพเนนต์
-
-    - กำหนด style ให้แตกต่างกันระหว่าง Android และ iOS
-      - Platform
+    - กำหนด Style แบบ Array
   
   */
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.textTitle}>Hello World</Text>
-      <Text style={styles.textPlain}>Hello World</Text>
+    <View>
+      
+      <Text style={
+        [styles.textBold, 
+        styles.textBold, 
+        styles.text20]
+      }>Hello World</Text>
+
+      <Text style={
+        [styles.textBlack, styles.text16]
+      }>Hello World</Text>
+
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    backgroundColor: 'lightgray',
-    padding: 20,
-    ...Platform.select({ /* ต้องมี (Spread) ข้างหน้า platform */
-      android: {
-        paddingTop: 30,
-        ios: {
-          paddingTop: 45
-        }
-      }
-    })
+  textBold: {
+    fontWeight: 'bold'
   },
-  textTitle: {
-    color: 'blue',
-    fontSize: 20,
-    fontWeight: 'bold',
-    marginTop: 15,
+  textBlue: {
+    color: 'blue'
   },
-  textPlain: {
-    fontSize: 16,
+  textBlack: {
+    color: 'black'
   },
-});
+  text20: {
+    fontSize: 20
+  },
+  text16: {
+    fontSize: 16
+  }
+})
 
 export default App;
---
+++ HelloApp/src/styles/common-styles.js
@@ -0,0 +1,20 @@
+import {
+  StyleSheet
+} from 'react-native'
+
+const commonStyles = StyleSheet.create({
+  container: {
+    padding: 20
+  },
+  textTitle: {
+    color: 'blue',
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginTop: 15,
+  },
+  textPlain: {
+    fontSize: 16
+  }
+})
+
+export default commonStyles
