d1e9baad77027c6ba5ec1318db9f8fbb225aded8
ChampKos <32265715+champseo2017@users.noreply.github.com>
2022-10-01 19:39:16 +0700

    การใช้ Flexbox ร่วมกับ React Native 1

--- HelloApp/App.js
+++ HelloApp/App.js
@@ -11,6 +11,21 @@ import commonStyles from './src/styles/common-styles'
 
 const App = () => {
 
+  /* 
+    การใช้ Flexbox ร่วมกับ React Native
+    - flexDirection React Native ค่าดีฟอลต์คือ column เรียงจากบนลงล่าง
+
+    - container ชั้นนอกสุดหรือกรอบหน้าจอ
+      - ควรกำหนดเป็น flex:1 เพื่อให้มีขนาดเต็มจอ
+
+    - หากจะเรียงข้อมูลแต่ละรายการ (items) จากบน
+    ลงล่าง กำหนด flexDirection:'column'
+    ให้กับ container หรือไม่กำหนดเลยเพราะเป็นค่าดีฟอลต์สำหรับ react native
+
+    - เรียงจากซ้ายไปขวา ให้กำหนด flexDirection: 'row' กรณีนี้ต้องระบุเสมอ
+  
+  */
+
   return (
     <View style={commonStyles.container}>
       
